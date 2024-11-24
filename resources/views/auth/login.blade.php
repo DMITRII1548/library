7ec8d02cf90df7cbf0f7de3897eb2cc9bf68c4c2
DMITRII1548 <korsunov2006@list.ru>
2024-11-24 21:13:38 +0600

    Update login and password page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,6 +8,18 @@
 </head>
 <body>
     <div class="pole_and_register_and_login">
+        <div class="pole_login_background">
+            <div class="pole_login">
+                <div class="logotip"><a href="../glavna/index.html"><img src="./Без имени-1 4.png" alt=""></a></div>
+                <div class="pole_info_login">
+                    <h1>{{ __('messages.login.info.title')  }}</h1>
+                    <h2>{{ __('messages.login.info.description')  }}</h2>
+                </div>
+                <div class="login">
+                    <a href="{{ route('register', LaravelLocalization::setLocale()) }}"><button class="button_login">{{ __('messages.login.info.btn')  }}</button></a>
+                </div>
+            </div>
+        </div>
         <div class="pole_register">
             <div class="title_register"><h1>{{ __('messages.login.title') }}</h1></div>
             <div class="pole">
@@ -15,32 +27,20 @@
                     @csrf
                     <div class="pole_email">
                         <label for="input_email">{{ __('messages.fields.email.label') }}</label><br>
-                        <input name="email" class="input_email" type="email" placeholder="example@1.mail.ru" required>
+                        <input name="email" class="input_email" type="email" placeholder="example@1.mail.ru">
                         @error('email')
                             <p class="text-danger">{{ $message }}</p>
                         @enderror
                     </div>
                     <div class="pole_password">
                         <label for="input_password">{{ __('messages.fields.password.label') }}</label><br>
-                        <input name="password" class="input_password" type="password" placeholder="password!231452" required>
+                        <input name="password" class="input_password" type="password" placeholder="password!231452">
                         @error('password')
                             <p class="text-danger">{{ $message }}</p>
                         @enderror
                     </div>
                     <button type="submit" class="button_register">{{ __('messages.login.btn') }}</button>
                 </form>
-            </div>
-        </div>
-        <div class="pole_login_background">
-            <div class="pole_login">
-                <div class="logotip"><a href="{{ route('books.index') }}"><img src="{{ asset('assets/imgs/auth/logo.png') }}" alt=""></a></div>
-                <div class="pole_info_login">
-                    <h1>{{ __('messages.login.info.title') }}</h1>
-                    <h2>{{ __('messages.login.info.description') }}</h2>
-                </div>
-                <div class="login">
-                    <a href="{{ route('register', LaravelLocalization::setLocale()) }}" class="button_login">{{ __('messages.login.info.btn') }}</a>
-                </div>
             </div>
         </div>
     </div>
